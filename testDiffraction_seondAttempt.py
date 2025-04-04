26a3c3f310f65b51336826fd5f0eb96f8003b2a5
rf <riccardo.forti@elettra.eu>
2025-04-04 11:42:56 +0200

    O, it kinda works, but there's problems with multiple minima
    If lens1 is too big (as in our case, you would have one mimina in the center and one circular minimum. The only way I can see doing the integral in different regions is to split the map into halfs until the valid part of each map has a good filling ratio...That's gonna be fun

--- testDiffraction_seondAttempt.py
+++ testDiffraction_seondAttempt.py
@@ -75,6 +75,73 @@ def expandMap(map, maxExceptedValue, ranges, extensionMultiplier = 2, reductiona
 			newHalfSizes = [(max-min)*.5*extensionMultiplier for i, (min,max) in enumerate(ranges)]
 		else:
 			#we have the miminum inside the frame. Let's center it
+			#since when we zoom the value of the function will be reduced, if we zoom too much we might extend the "0" area outside of the zoom.
+			minVal = map[localMins[1],localMins[0]] / maxExceptedValue
+			expectedMaxVal = minVal / np.sqrt(map.size)
+			if expectedMaxVal < 1:
+				newHalfSizes = [(max-min)*.5/minVal for (min,max) in (ranges)]
+			else:
+				newHalfSizes = [(max-min)*.5/map.shape[i] for i, (min,max) in enumerate(ranges)]
+		return ((newCenters[0]-newHalfSizes[0],newCenters[0]+newHalfSizes[0]),(newCenters[1]-newHalfSizes[1],newCenters[1]+newHalfSizes[1]))
+
+		
+	((minx, maxx), (miny, maxy)) = boundingBox(mm)
+	centerX=(minx+maxx)/2
+	centerY=(miny+maxy)/2
+	indexes = [minx, maxx, miny, maxy]
+	signs = [-1,1,-1,1]
+	centers =[centerX,centerX,centerY,centerY]
+	limits = [0,len(map)-1,0,len(map[0])-1]
+	minValues = [minValues[0],minValues[0],minValues[1],minValues[1]]
+	pixelSizes = [pixelSizes[0],pixelSizes[0],pixelSizes[1],pixelSizes[1]]
+	newValues = []
+	for val, center, sign, limit, min, pixelSize in zip(indexes,centers,signs,limits,minValues,pixelSizes):
+		if val==limit:#acceptable range possibly extending outside of the current range?
+			newIndex = center + extensionMultiplier * (val - center)
+		else:
+			newIndex = val + sign * reductionadder
+		newValues.append(min + pixelSize * newIndex)
+	return ((newValues[0],newValues[1]),(newValues[2],newValues[3]))
+
+def expandMap_old(map, maxExceptedValue, ranges, extensionMultiplier = 2, reductionadder = 1, reductionMultiplier = 20):
+	'''returns a new search range for the estimate of the acceptable ranges of a map,
+	so that the new map (generated with an external function that uses the new search range) 
+	should contains more (and hopefully all the acceptable region(s?) of the function... Sorry, 
+	I haven't explained myself... Oh well, hopefully I'll remember what this actually does'''
+	mm = map < maxExceptedValue
+	minValues = [ranges[0][0], ranges[1][0]]
+	pixelSizes = [(max-min)/map.shape[i] for i, (min,max) in enumerate(ranges)]
+	if not np.any(mm):#no valid pixel?
+		# #Let's assume that there will be a valid value in the center, and just return the size of one pixel centered in the map
+		# pixelHalfSizes = [(max-min)*reductionadder*.5/map.shape[i] for i, (min,max) in enumerate(ranges)]
+		# mapCenter = [(max+min)/2 for (min,max) in ranges]
+		# return ((mapCenter[0]-pixelHalfSizes[0],mapCenter[0]+pixelHalfSizes[0]),(mapCenter[1]-pixelHalfSizes[1],mapCenter[1]+pixelHalfSizes[1]))
+		
+		#Let's find an estimate for the minimum, and let's move the range there
+		axes=[0,1]
+		limits = [[0,len(map)-1], [0,len(map[0])-1]]
+		onAnyLimit=False
+		newCenters = []
+		localMins = np.unravel_index(np.argmin(np.where(np.isnan(map), np.inf, map)), map.shape)
+		localMins = [localMins[1],localMins[0]]
+		for min, axis, limit in zip(localMins, axes,limits):
+			onLimit = -1
+			for i, lim in enumerate(limit):
+				if min == lim:
+					onLimit = i
+			if onLimit != -1:
+				#let's move the range half a range to the direction of the mimimum
+				onAnyLimit = True
+				newCenters.append(map.shape[onLimit] * (onLimit))
+			else:
+				newCenters.append(min)
+			newCenters[-1] = newCenters[-1] * pixelSizes[axis] + minValues[axis]
+
+		if onAnyLimit:
+			#let's not reduce the size of the ranges. It's better to first have an estimate of the position of the mimimum, and then reduce the size to center it
+			newHalfSizes = [(max-min)*.5*extensionMultiplier for i, (min,max) in enumerate(ranges)]
+		else:
+			#we have the miminum inside the frame. Let's center it
 			newHalfSizes = [(max-min)*reductionMultiplier*.5/map.shape[i] for i, (min,max) in enumerate(ranges)]
 		return ((newCenters[0]-newHalfSizes[0],newCenters[0]+newHalfSizes[0]),(newCenters[1]-newHalfSizes[1],newCenters[1]+newHalfSizes[1]))
 
@@ -97,17 +164,19 @@ def expandMap(map, maxExceptedValue, ranges, extensionMultiplier = 2, reductiona
 		newValues.append(min + pixelSize * newIndex)
 	return ((newValues[0],newValues[1]),(newValues[2],newValues[3]))
 		
-def isMapOk(map, maxExceptedValue,fillRatio):
+def MapStatus(map, maxExceptedValue,fillRatio):
 	mm = map < maxExceptedValue
 	if not np.any(mm):
-		return False
+		return "noVal"
 	((minx, maxx), (miny, maxy)) = boundingBox(mm)
 	limits = np.array([0,len(map)-1,0,len(map[0])-1])
 	indexes = np.array([minx, maxx, miny, maxy])
 	if np.any(limits==indexes):
-		return False
+		return "notComplete"
 	validRatio = np.count_nonzero(mm) / map.size
-	return validRatio > fillRatio
+	if validRatio < fillRatio:
+		return "tooSmall"
+	return "ok"
 
 
 # def q(ranges,sizes):
@@ -158,9 +227,10 @@ def expandInFreeSpace_separated(U0, xy0_ranges,z0=0, k=1):
 	returns the convolution U1(x1,y1,z1) of the field U0(x,y,z=z0) with h(x,y,z) to obtain the value of U1(x,y,z)
 	the integral is calculated in the specified ranges
 	'''
-	nOfSamplesPerDimension = 100
-	def get_xyz0(ranges):
-		X,Y = np.meshgrid(*[np.linspace(min,max,nOfSamplesPerDimension)for (min,max) in ranges])
+	nOfSamplesPerDimension = 200
+	nOfSamplesForMapSearch = 50
+	def get_xyz0(ranges, sizes = nOfSamplesForMapSearch):
+		X,Y = np.meshgrid(*[np.linspace(min,max,sizes)for (min,max) in ranges])
 		Z=z0 * np.ones_like(X)
 		return np.stack((X,Y,Z), axis=2)
 	def alternateRange(l,i):
@@ -171,20 +241,24 @@ def expandInFreeSpace_separated(U0, xy0_ranges,z0=0, k=1):
 	scale=1
 	xyz0 = get_xyz0(xy0_ranges)
 	def U1(xyz1):
+		if len(xyz1.shape) == 2:#list of points instead of a grid of points?
+			xyz1 = np.reshape(xyz1, (xyz1.shape[0],1,xyz1.shape[1]))
 		nonlocal xyz0, scale, xy0_ranges
 		newRange = xy0_ranges
 		integral = np.zeros(xyz1.shape[:-1], dtype=np.complex128)
 		plotGradiens=False
-		gradLim =np.pi/4
+		gradLim =np.pi/2
 		for i in range(len(xyz1)):
 			for j in alternateRange(len(xyz1[i]),i):
-				if i==1:
+				if i==5 and j==9:
 					i += 0
 				newRange = xy0_ranges
 				xyz0 = get_xyz0(newRange)
-				repeats = 20
+				repeats = 10
 				# newRange = ((newRange[0][0]-xyz1[i, j, 0],newRange[0][1]-xyz1[i, j, 0]),(newRange[1][0]-xyz1[i, j, 1],newRange[1][1]-xyz1[i, j, 1]))
 				# newRange = ((newRange[0][0]+xyz1[i, j, 0],newRange[0][1]+xyz1[i, j, 0]),(newRange[1][0]+xyz1[i, j, 1],newRange[1][1]+xyz1[i, j, 1]))
+				foundOneGoodRange = False
+				foundOneSmallRange = False
 				while repeats>0:
 					allImpulseResponses, validSections = impulseExpansionInFreeSpace_separated(xyz1[i, j, :], U0, xyz0, k)
 					if plotGradiens:
@@ -194,8 +268,21 @@ def expandInFreeSpace_separated(U0, xy0_ranges,z0=0, k=1):
 						plt.imshow(validSections/gradLim)
 						'''
 						plt.show()
-					if not isMapOk(validSections, gradLim, 0.2):
-						newRange = expandMap(validSections, gradLim, newRange, 1.3,20)
+					ms = MapStatus(validSections, gradLim, 0.2)
+					if ms != "ok":
+						if ms != "noVal":
+							if ms == "tooSmall":
+								if foundOneSmallRange:
+									break
+								foundOneSmallRange = True
+							#we found some good values, but it's still not perfect
+							foundOneGoodRange = True
+						elif foundOneGoodRange:
+							#while zooming out to get all the "valid" values, we ended up increasing the difference in values too 
+							#much => we can't obtain a map with all the good values (or better, there were no good values to begin 
+							#with, and we found something only because we zoomed so much). Let's just break
+							break
+						newRange = expandMap(validSections, gradLim, newRange, 1.5,8)
 						xyz0 = get_xyz0(newRange)
 					else:
 						break
@@ -210,6 +297,12 @@ def expandInFreeSpace_separated(U0, xy0_ranges,z0=0, k=1):
 					# allImpulseResponses, validSections = impulseExpansionInFreeSpace_separated(xyz1[i, j, :], U0, xyz0, k)
 					# validSections = (np.logical_not(np.isnan(validSections))).astype(float)
 				
+				xyz0 = get_xyz0(newRange, nOfSamplesPerDimension)
+				
+				allImpulseResponses, validSections = impulseExpansionInFreeSpace_separated(xyz1[i, j, :], U0, xyz0, k)
+				# validSections[np.isnan(validSections)] = np.inf
+				# minValidSections = np.min(validSections)
+				# validSections = (validSections//gradLim < (minValidSections // gradLim) + 1).astype(float)
 				validSections = (validSections < gradLim).astype(float)
 				integral[i][j] = np.sum(allImpulseResponses * validSections) / gridSize * np.prod(np.array([(max-min)*scale for min,max in newRange]))
 
@@ -304,13 +397,13 @@ def gridFunction_separated(startingFunction, ranges, z, nOfValuesPerDimension = 
 
 f0 = 25.5e-3
 pow = 1
-R0 = 16e-3
+R0 = 10e-3
 f1 = 200e-3
 R1 = 27e-3
 lam = 399e-9#1e-6#f0/3#399e-9
 k=2*np.pi/lam
 
-z0=f0+50
+z0=f0+50#+3e-6
 z1=z0+f1
 z2=z1+f1
 effectiveR_lens0=R0
@@ -323,6 +416,11 @@ def U0_separated(k):
 		return cosAlpha / r**2, k*r
 	return U0
 
+def dipoleField_separated(xyz, k):
+	r=np.linalg.norm(xyz, axis=-1)
+	theta = np.arcsin(xyz[...,1]/r)
+	cosAlpha = xyz[...,2]/r
+	return (1+np.cos(2*theta))*cosAlpha / r**2, k*r
 
 U0=U0_separated(k)
 U_afterLens0=passThroughLens_separated(U0, k, f0, R0)
@@ -333,9 +431,10 @@ U_afterLens0=passThroughLens_separated(U0, k, f0, R0)
 U_beforeLens1=expandInFreeSpace_separated(U_afterLens0, 
 							[[-effectiveR_lens0,effectiveR_lens0],[-effectiveR_lens0,effectiveR_lens0]], z0, k)
 # plot2D_function(addStaticY(U_beforeLens1,0), [0,effectiveR_lens0*.1],[z0+f0*.1,z0+f0*1.9],50,50, "U_beforeLens1_xz")
-# plot1D_function(addStaticYZ(U_beforeLens1,0,z0+f0), [0,effectiveR_lens0*.1],100, "U_beforeLens1_xz")
+# plot1D_function(addStaticYZ(U_beforeLens1,0,z0+f0+lam*50), [0,1e-5],100, "U_beforeLens1_xz")
+# plot1D_function(addStaticYZ(U_beforeLens1,0,z0+f0), [0,1e-5],100, "U_beforeLens1_xz")
 # plot2D_function(addStaticZ(U_beforeLens1,z0+f0), [-0,1e-5],[-0,1e-5],50,50, "U_beforeLens1_xy")
-plot2D_function(addStaticZ(U_beforeLens1,z0+f0*1.0002), [-1e-7,1e-7],[-1e-7,1e-7],50,50, "U_beforeLens1_xy")
+plot2D_function(addStaticZ(U_beforeLens1,z1), [-0,R0*.6],[-0,R0*.6],10,10, "U_beforeLens1_xy")
 
 a=0
 # U_afterLens1=passThroughLens_separated(U_beforeLens1, k, f1, R1)
