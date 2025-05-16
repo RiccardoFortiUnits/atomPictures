fixed magnification, added defocus and elliptical average

Camera.py
@@ -135,6 +135,17 @@ def fft2_butBetter(image, imageSize, transformSize, transform_n = None, paddingV
 	CorrectSizedImage = f(x,y)
 	return np.fft.fft2(CorrectSizedImage)
 		
+def mirrorSymmetricImage(image, mirrorFirstLines = False):
+	'''
+	given an image f(x>=0,y>=0), it mirrors it along the x and y axes.
+	if mirrorFirstLines is false, the first row and column are treated as the values of f(0,y) and f(x,0), and thus they won't be mirrored.
+	'''
+	toFlip = image if mirrorFirstLines else image[:,1:]
+	image = np.concatenate((np.flip(toFlip,axis=1), image), axis=1)
+	toFlip = image if mirrorFirstLines else image[1:,:]
+	image = np.concatenate((np.flip(toFlip,axis=0), image), axis=0)
+	return image
+
 def filterScatterToGrid(x,y,z,gridPoints):
 	mins = [np.min(x), np.min(y)]
 	maxs = [np.max(x), np.max(y)]
@@ -220,6 +231,12 @@ class cameraAtomImages:
 		x,y = np.meshgrid(*[np.arange(0,image.shape[i]) for i in range(2)], indexing="ij")
 		center = np.unravel_index(np.argmax(np.abs(image)), image.shape)
 		parameters, _ = curve_fit(Ar.Gaussian_2D, np.vstack((x.flatten(),y.flatten())), image.flatten(), p0=[np.max(image), *center, 1,1, 0])
+		# print(parameters[3], parameters[4])
+		return parameters[1], parameters[2]
+	@staticmethod
+	def findCenterOfGaussianSetOfImages(x,y,z):
+		center = np.unravel_index(np.argmax(np.abs(z)), z.shape)[-2:]
+		parameters, _ = curve_fit(Ar.Gaussian_2D, np.vstack((x.flatten(),y.flatten())), z.flatten(), p0=[np.max(z), *center, 1,1, 0])
 		return parameters[1], parameters[2]
 	
 
@@ -265,13 +282,13 @@ class cameraAtomImages:
 		self._tweezerPixels = np.round(centers).astype(int)
 
 	@staticmethod
-	def azimuthal_average(x,y,z, center = None, n_bins=None):
+	def azimuthal_average(x,y,z, center = None, n_bins=None, dividestandardBin = 1):
 		if center is None:
 			idx = np.unravel_index(np.argmax(z), x.shape)
 			center = x[idx],y[idx]
 		x0,y0 = center
 		if n_bins is None:
-			n_bins = int(np.ceil(np.hypot(z.shape[1],z.shape[2]) / 2))
+			n_bins = int((z.shape[1] + z.shape[2]) * dividestandardBin)
 
 		r = np.hypot(x-x0,y-y0) 
 		r_bins = np.linspace(r.min(),r.max(),n_bins+1)
@@ -285,6 +302,21 @@ class cameraAtomImages:
 		z_avg[unique] /= count
 
 		return r_bins_center, z_avg
+	@staticmethod
+	def elliptical_average(x,y,z, angle, axisRatio, center = None, n_bins=None, dividestandardBin = 1):
+		'''
+		angle:angle of the axis that will not be normalized (i.e,, which is already normalized)
+		axisRatio = length of the normalized axis / length of the perpendicular axis
+		'''
+		if center is None:
+			idx = np.unravel_index(np.argmax(z), x.shape)
+			center = x[idx],y[idx]
+		x0,y0 = center
+		x,y=x-x0,y-y0
+		x,y=x*np.cos(angle) + y*np.sin(-angle), y*np.cos(angle) - x*np.sin(-angle)
+		y *= axisRatio
+		z /= axisRatio
+		return cameraAtomImages.azimuthal_average(x,y,z, center = (0,0), n_bins=n_bins, dividestandardBin=dividestandardBin)
 
 	@property
 	def tweezerPositions(self):
@@ -313,7 +345,18 @@ class cameraAtomImages:
 	def getAtomROI_fromBooleanArray(self, roi, array, averageOnAtoms = False):
 		imgs, atoms = np.where(array)
 		return self.getAtomROIOnImages(self.images, self.tweezerPixels, roi, imgs, atoms, averageOnAtoms)
-	
+	def getAtomCoordinates(self, roi, pixelSize = 1, atomsIdx = None):
+		'''
+		returns the coordinates of the atoms in the specified roi. 
+		if atomsIdx is not specified, all the valid indexes will be considered
+		'''
+		if atomsIdx is None:
+			atomsIdx = np.arange(0, len(self.tweezerPixels.T))
+		x,y = np.meshgrid(*[np.arange(0,roi) for _ in range(2)], indexing="ij")
+		x = (x[None,:,:] - roi//2 - (self.tweezerPositions - self.tweezerPixels)[0][:,None,None]) * pixelSize
+		y = (y[None,:,:] - roi//2 - (self.tweezerPositions - self.tweezerPixels)[1][:,None,None]) * pixelSize
+		
+		return x[atomsIdx,:,:],y[atomsIdx,:,:]
 	@staticmethod
 	def getAtomROIOnImages(images, tweezerPixels, roi, imageIdx = None, atomIdx = None, averageOnAtoms = False):
 		'''
@@ -430,8 +473,8 @@ class pixelGrid:
 		coordinates = np.argwhere(self.pixels > 0)
 		counts = self.pixels[self.pixels > 0].astype(int)
 		coordinates = np.repeat(coordinates, counts, axis=0).astype(float)
-		coordinates[:,0] = (coordinates[:,0] / (self.nofXpixels - 1) - .5) * self.xsize * self.magnification
-		coordinates[:,1] = (coordinates[:,1] / (self.nofYpixels - 1) - .5) * self.ysize * self.magnification
+		coordinates[:,0] = (coordinates[:,0] / (self.nofXpixels - 1) - .5) * self.xsize
+		coordinates[:,1] = (coordinates[:,1] / (self.nofYpixels - 1) - .5) * self.ysize
 		return coordinates
 	def fillFromLens(self, rawPhotonPositions):
 		'''
@@ -439,6 +482,8 @@ class pixelGrid:
 
 		returns a dictionary with some extra info (though the default pixel grid only returns a non-empty dictionary if there's no photon to work on)
 		'''
+		magnification = np.array([self.magnification,self.magnification,1])
+		rawPhotonPositions = rawPhotonPositions * magnification[None,:rawPhotonPositions.shape[1]]
 		if len(rawPhotonPositions) == 0:
 			return {"warning:" : "no photons hitting the lens"}
 		if len(rawPhotonPositions[0]) == 3:
@@ -449,16 +494,16 @@ class pixelGrid:
 		np.add.at(self.pixels, (x_normalized, y_normalized), 1)
 		return {}
 	def fillFromOtherGrid(self, inputGrid : 'pixelGrid'):
-		psfPositions = self.PSF(inputGrid.getRawPositions())
+		psfPositions = self.PSF(inputGrid.getRawPositions() * self.magnification)
 		x_normalized, y_normalized = self._normalizeCoordinate(psfPositions[:,0], psfPositions[:,1])
 		np.add.at(self.pixels, (x_normalized, y_normalized), 1)
 	def clear(self):
 		self.pixels = np.zeros((self.nofXpixels,self.nofYpixels))
 
 class cMosGrid(pixelGrid):
-	def __init__(self, xsize, ysize, nofXpixels, nofYpixels, PSF, noisePictureFilePath, imageStart = (0,0), imageSizes = None):
+	def __init__(self, xsize, ysize, nofXpixels, nofYpixels, PSF, noisePictureFilePath, imageStart = (0,0), imageSizes = None, magnification = 1):
 
-		super().__init__(xsize, ysize, nofXpixels, nofYpixels, PSF)
+		super().__init__(xsize, ysize, nofXpixels, nofYpixels, PSF, magnification)
 		self.setRandomPixelNoises(noisePictureFilePath, imageStart, imageSizes)
 
 	def setRandomPixelNoises(self, noisePictureFilePath, imageStart = (0,0), imageSizes = None):
@@ -505,16 +550,16 @@ class refreshing_cMosGrid(cMosGrid):
 	to a "bad" pixel always being in a certain position, or other similar problems.
 	This subclass just gets random pixel values for each imaging
 	'''	
-	def __init__(self, xsize, ysize, nofXpixels, nofYpixels, PSF, noisePictureFilePath, imageStart = (0,0), imageSizes = None):
-		super().__init__(xsize, ysize, nofXpixels, nofYpixels, PSF, noisePictureFilePath, imageStart, imageSizes)
+	def __init__(self, xsize, ysize, nofXpixels, nofYpixels, PSF, noisePictureFilePath, imageStart = (0,0), imageSizes = None, magnification = 1):
+		super().__init__(xsize, ysize, nofXpixels, nofYpixels, PSF, noisePictureFilePath, imageStart, imageSizes, magnification)
 		self.pixelNoises = self.pixelNoises.flatten()
 		
 	def addNoise(self):
 		self.pixels += np.random.choice(self.pixelNoises, self.pixels.shape, )
 
 class strayLight_cMosGrid(refreshing_cMosGrid):
-	def __init__(self, xsize, ysize, nofXpixels, nofYpixels, PSF, noisePictureFilePath, avoidanceRoi = 5, tweezerMinPixelDistance = 10, atomPeakMinValue = 1):
-		pixelGrid.__init__(self, xsize, ysize, nofXpixels, nofYpixels, PSF)
+	def __init__(self, xsize, ysize, nofXpixels, nofYpixels, PSF, noisePictureFilePath, avoidanceRoi = 5, tweezerMinPixelDistance = 10, atomPeakMinValue = 1, magnification = 1):
+		pixelGrid.__init__(self, xsize, ysize, nofXpixels, nofYpixels, PSF, magnification)
 		self.setRandomPixelNoises(noisePictureFilePath, avoidanceRoi, tweezerMinPixelDistance, atomPeakMinValue)
 
 	def setRandomPixelNoises(self, noisePictureFilePath, avoidanceRoi = 5, tweezerMinPixelDistance = 10, atomPeakMinValue = 1):
@@ -952,7 +997,7 @@ class experimentViewer:
 		quiverLength = maxRange / 20
 		min_bounds = (max_bounds + min_bounds) / 2 - maxRange / 2
 		max_bounds = min_bounds + maxRange
-		for atom_idx in range(self.lastPositons.shape[1]):
+		for atom_idx in range(1):#self.lastPositons.shape[1]):
 			ax.plot(self.lastPositons[:, atom_idx, 0], self.lastPositons[:, atom_idx, 1], self.lastPositons[:, atom_idx, 2], label=f'Atom {atom_idx+1}')
 			ax.scatter(*self.lastPositons[0, atom_idx, :], s=20)
 			if hasattr (self, "tweezerPositions"):
@@ -970,7 +1015,7 @@ class experimentViewer:
 		# 				directions = self.lastGeneratedPhotons[laserHits[h]] * quiverLength
 		# 				ax.quiver(position[0], position[1], position[2], 
 		# 						directions[0], directions[1], directions[2], color='red')
-					# ax.quiver(self.lastPositons[time_idx, atom_idx, 0], self.lastPositons[time_idx, atom_idx, 1], self.lastGeneratedPhotons[time_idx, atom_idx, 0])
+		# 			# ax.quiver(self.lastPositons[time_idx, atom_idx, 0], self.lastPositons[time_idx, atom_idx, 1], self.lastGeneratedPhotons[time_idx, atom_idx])
 
 		ax.set_xlabel('X Position (m)')
 		ax.set_ylabel('Y Position (m)')
@@ -1268,7 +1313,39 @@ if __name__ == '__main__':
 	# 				print(f"{metadata_key} not found in {file}")
 	# update_metadata_in_h5_files("D:/simulationImages/blurs", "range", 4.6e-6 * 8)
 
-	exp = experimentViewer()
-	exp.loadAcquisition("d:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/simulation/simulation_4.h5")
-	exp.getScatteredPhotons(6e-6)
+	# exp = experimentViewer()
+	# exp.loadAcquisition("d:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/simulation/simulation_4.h5")
+	# exp.getScatteredPhotons(6e-6)
+
+	# img = np.array([[1,2,3],[4,5,6],[7,8,9]])
+	# print(mirrorSymmetricImage(img))
+	# print(mirrorSymmetricImage(img, True))
+	# blur_z0,metadata = load_h5_image("d:/simulationImages/blurs/399nm/camera_atomZ=0.00e+00.h5", returnMetadata=True)
+	# blur_z0 = np.abs(blur_z0)**2
+	# blur_z0 = blur_z0[blur_z0.shape[0]//2,:]/np.sum(blur_z0)
+	# size = metadata["requestedRange"]
+	# pixelSize = 4.6e-6
+	# # Create a pixel response function (rectangular function of width pixelSize)
+	# x = np.linspace(-size/2, size/2, blur_z0.shape[0])
+	# pixel_response = np.where(np.abs(x) <= pixelSize/2, 1, 0)
+	# plt.plot(x, pixel_response * np.max(blur_z0), label='pixel')
+	# pixel_response = pixel_response / np.sum(pixel_response)  # Normalize
+
+	# # Convolve blur_z0 with the pixel response
+	# blur_z0_pixel = np.convolve(blur_z0, pixel_response, mode='same')
+	# def gaussian(x, A, mu, sigma, offset):
+	# 	return A * np.exp(-2 * ((x - mu) / sigma) ** 2) + offset
+
+	# # Initial guess: amplitude, mean, stddev, offset
+	# p0 = [np.max(blur_z0_pixel), 0, pixelSize, np.min(blur_z0_pixel)]
+	# params, cov = curve_fit(gaussian, x, blur_z0_pixel, p0=p0)
+
+	# fit_curve = gaussian(x, *params)
+	# plt.plot(x, fit_curve, label='Gaussian fit')
+	# print("Fitted parameters: A = {:.3g}, mu = {:.3g}, sigma = {}, offset = {:.3g}".format(*params))
+	# plt.plot(x, blur_z0, label='blur function (camera space)')
+	# plt.plot(x, blur_z0_pixel, label='Convolved with pixel')
+	
+	# plt.legend()
+	# plt.show()
 	pass

analisys_simulationImages.py
@@ -14,6 +14,9 @@ from scipy.stats import poisson
 from scipy.stats import norm
 import scipy.optimize as opt
 import ArQuS_analysis_lib as Ar
+import matplotlib.patches as patches
+
+from scipy.ndimage import maximum_filter
 
 
 '''----------------------------------------general values----------------------------------------'''
@@ -22,10 +25,29 @@ pixelSize = 4.6e-6 / 8
 
 coordinates = []#[exp_x,exp_y,exp_z]]
 names = []#"experiment"]
-nOfImages = 100
+valuesForEllpiticFit = []
+nOfImages = None
+
+def gaussian(x, A, sigma, offset):
+	return A * np.exp(-2 * ((x) / sigma) ** 2) + offset
+def gaussian2D(xy, A,x0,y0, sigma_x, sigma_y, offset, angle = 0):
+	x,y=xy[0]*np.cos(angle) + xy[1]*np.sin(angle), xy[1]*np.cos(angle) - xy[0]*np.sin(angle)
+
+	return A * gaussian(x-x0,1,sigma_x,0) * gaussian(y-y0,1,sigma_y,0) + offset
+def getFittedGaussian(x,y):
+	p0 = [np.max(z), x[np.argmax(y.flatten())], 1, np.min(y)]
+	params, cov = curve_fit(gaussian, r*8, az, p0=p0)
+	return params
+def getFittedGaussian2D(x,y,z):
+	center = np.unravel_index(np.argmax(z.flatten()), x.shape)
+	p0 = [np.max(z), x[*center], y[*center], (x.max()-x.min())/8, (y.max()-y.min())/8, np.min(z),0]
+	params, cov = curve_fit(gaussian2D, np.row_stack((x.flatten(),y.flatten())), z.flatten(), p0=p0)
+	return params
+	
+	
 '''----------------------------------------experimental data----------------------------------------'''
 
-exp_files = []#["D:/simulationImages/real images/171_10Tweezer_inTrap_12us_2Images"]
+exp_files = ["D:/simulationImages/real images/171_10Tweezer_inTrap_12us_2Images"]
 internalPath = [f"images/Orca/fluorescence {i}/frame" for i in [1,2]]
 for exp_file in exp_files:
 	exp_cai = Camera.doubleCameraAtomImage(exp_file, exp_file, *internalPath, maxPictures=nOfImages)
@@ -34,66 +56,164 @@ for exp_file in exp_files:
 
 	sti = exp_cai.getSurelyTrappedAtoms(photonThreshold = 8, roi = 3)
 	exp_z = exp_cai.first.getAtomROI_fromBooleanArray(comparisonRoi, sti, averageOnAtoms=True)
-	exp_x,exp_y = np.meshgrid(*[np.arange(0,comparisonRoi) for _ in range(2)], indexing="ij")
-	exp_x = (exp_x[None,:,:] - comparisonRoi//2 - (exp_cai.first.tweezerPositions - exp_cai.first.tweezerPixels)[0][:,None,None]) * pixelSize
-	exp_y = (exp_y[None,:,:] - comparisonRoi//2 - (exp_cai.first.tweezerPositions - exp_cai.first.tweezerPixels)[1][:,None,None]) * pixelSize
+	exp_x,exp_y = exp_cai.first.getAtomCoordinates(comparisonRoi, pixelSize)
+	A,x0,y0,sigma_x,sigma_y,offset,angle = getFittedGaussian2D(exp_x,exp_y,exp_z)
+	if sigma_x > sigma_y:
+		sigma_x,sigma_y = sigma_y,sigma_x
+		angle += np.pi/2
+	valuesForEllpiticFit.append([sigma_x,sigma_y, angle])
+	print(f"exp_file: {exp_file}, A = {A}, x0 = {x0}, y0 = {y0}, sigma_x = {sigma_x}, sigma_y = {sigma_y}, offset = {offset}, angle = {angle}")
+	exp_x = exp_x - x0
+	exp_y = exp_y - y0
 	coordinates.append([exp_x,exp_y,exp_z])
 	names.append(exp_file.split("/")[-1])
 
 '''----------------------------------------simulation data----------------------------------------'''
+'''
 sim_files = [
-#   "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/pictures",
-#   "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray_z_lattice/pictures",
-#   "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray_atomUnmovable/pictures",
+  "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/12us_pictures",
   "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/5us_pictures",
-  "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/6us_pictures",
-  "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/10us_pictures",
-  "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/pictures",
-  ]
+  "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray_z_lattice/pictures",
+  "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray_atomUnmovable/pictures",
+]
+names += ["12us", "5us", "12us_z-lattice", "12us_fixedAtom"]
+'''
+sim_files = [
+	# "D:/simulationImages/magnified_Yt171_20us_10tweezerArray/correctMagnification_outOfFocus0.0e+00m_7us_pictures",
+	"D:/simulationImages/magnified_Yt171_20us_10tweezerArray/correctMagnification_outOfFocus0.0e+00m_12us_pictures",
+	# "D:/simulationImages/magnified_Yt171_20us_10tweezerArray/correctMagnification_outOfFocus0.0e+00m_20us_pictures",
+	# "D:/simulationImages/magnified_Yt171_20us_10tweezerArray_atomUnmovable/correctMagnification_outOfFocus0.0e+00m_12us_pictures",
+	# "D:/simulationImages/magnified_Yt171_20us_10tweezerArray/correctMagnification_outOfFocus0.0e+00m_12us_pictures_10reduction",
+	# "D:/simulationImages/magnified_Yt171_20us_10tweezerArray_z_lattice/correctMagnification_outOfFocus0.0e+00m_12us_pictures_10reduction",
+	# "D:/simulationImages/magnified_Yt171_20us_10tweezerArray_atomUnmovable/correctMagnification_outOfFocus0.0e+00m_12us_pictures_10reduction",
+
+	# "D:/simulationImages/magnified_Yt171_20us_10tweezerArray/correctMagnification_outOfFocus0.0e+00m_7us_pictures",
+	# "D:/simulationImages/magnified_Yt171_20us_10tweezerArray/correctMagnification_outOfFocus0.0e+00m_12us_pictures_10reduction",
+#   "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/12us_pictures",
+#   "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/outOfFocus1.0e-06m_12us_pictures",
+  "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/outOfFocus2.0e-06m_12us_pictures",
+#   "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/outOfFocus3.0e-06m_12us_pictures",
+  "D:/simulationImages/correctScattering_Yt171_12us_10tweezerArray/outOfFocus4.0e-06m_12us_pictures",
+]
+names += ["std simulation", "offset 2um", "offset 4um"]#,"simulation_smallerPixel", "simul. un-movable atom_smallerPixel"]           #[f"simulation" for i in range(len(sim_files))]#[f"offset {i}um" for i in [4]]#[0,4,1,2,3]]
+#'''
 for sim_file in sim_files:
 	sim_cai = Camera.cameraAtomImages(sim_file, maxPictures=nOfImages)
-	sim_cai.calcTweezerPositions(tweezerMinPixelDistance = 10 * pixelScale, atomPeakMinValue = 1.3 / pixelScale)
+	metadata = list(sim_cai.metadata.values())[0]
+	pixelScale = int(pixelSize * 8 / metadata["camera_pixelSize"])
+	expectedPeakValue = metadata["number of photons hitting grid 0"] * 2.8 / 380
+	comparisonRoi = int(comparisonRoi * pixelScale)
+	sim_cai.calcTweezerPositions(tweezerMinPixelDistance = 10 * pixelScale, atomPeakMinValue = expectedPeakValue / pixelScale)
 	sim_z = sim_cai.getAtomROI(comparisonRoi, averageOnAtoms=True)
-	sim_x,sim_y = np.meshgrid(*[np.arange(0.,comparisonRoi, dtype=float) for _ in range(2)], indexing="ij") 
-	# tweezerCenters = np.array([el["tweezer_centers"][0][:2] for el in sim_cai.metadata.values()])
-	pixelSize = list(sim_cai.metadata.values())[0]["camera_pixelSize"]
-	# tweezerCenters /= pixelSize
-	# tweezerCenters[:,0] *= -1#for how the simulation works now, the x axis gets flipped when acquiring the image, so I put a negative sign in the position of the tweezer
-	sim_x = (sim_x[None,:,:] - comparisonRoi//2 - (sim_cai.tweezerPositions - sim_cai.tweezerPixels)[0][:,None,None]) * pixelSize
-	sim_y = (sim_y[None,:,:] - comparisonRoi//2 - (sim_cai.tweezerPositions - sim_cai.tweezerPixels)[1][:,None,None]) * pixelSize
+	sim_x,sim_y = sim_cai.getAtomCoordinates(comparisonRoi, pixelSize / pixelScale)
+	A,x0,y0,sigma_x,sigma_y,offset, angle = getFittedGaussian2D(sim_x,sim_y,sim_z)
+	if sigma_x > sigma_y:
+		sigma_x,sigma_y = sigma_y,sigma_x
+		angle += np.pi/2
+	valuesForEllpiticFit.append([sigma_x,sigma_y, angle])
+	print(f"sim_file: {sim_file}, A = {A}, x0 = {x0}, y0 = {y0}, sigma_x = {sigma_x}, sigma_y = {sigma_y}, offset = {offset}, angle = {angle}")
+	
+	sim_x = sim_x - x0
+	sim_y = sim_y - y0
+	
 	coordinates.append([sim_x,sim_y,sim_z])
-	names.append(sim_file.split("/")[-1])
+	comparisonRoi = int(np.round(comparisonRoi / pixelScale))
+	# names.append(sim_file.split("/")[-1])
 
+	# imageSize = metadata["camera_pixelSize"] * np.array( metadata["camera_pixelNumber"])
+	# obtainedTweezerPositions = ((sim_cai.tweezerPositions / np.array([sim_cai.images.shape[1],sim_cai.images.shape[2]])[:,None])-.5)*imageSize[:,None]
+	# actualTweezerPositions = metadata["tweezer_centers"][:,:2]
+	# actualTweezerPositions = np.column_stack((actualTweezerPositions[:,1], -actualTweezerPositions[:,0])).T
+	# errors = np.linalg.norm((actualTweezerPositions - obtainedTweezerPositions), axis=0)
+	# print(f"{sim_file}: average error in the positions of the tweezers: {np.mean(errors):.2e} m ({np.mean(errors)/pixelSize:.3f} pixels)")
+	# print(f"{sim_file}: maximum error in the positions of the tweezers: {np.max(errors):.2e} m ({np.max(errors)/pixelSize:.3f} pixels)")
+	
 '''
 for i in range(len(sim_files)):
 	coordinates[i][2] = coordinates[i][2] / np.max(coordinates[i][2])
 '''
+'''----------------------------------------azimutal average----------------------------------------'''
+# plt.figure("Elliptic Fits")
+# plt.scatter(0,0,c="red")
+# ax = plt.gca()
+# for i, (sigma_x, sigma_y, angle, x0,y0) in enumerate(valuesForEllpiticFit):
+# 	ellipse = patches.Ellipse(
+# 		(0,0), 
+# 		width=2*sigma_x, 
+# 		height=2*sigma_y, 
+# 		angle=np.degrees(-angle), 
+# 		fill=False, edgecolor='C{}'.format(i), label=names[i] if i < len(names) else None
+# 	)
+# 	plt.gca().add_patch(ellipse)
+
+# idealPsf = patches.Circle((0,0), radius = 270e-9, fill=False, label="ideal PSF", color="black", linestyle='--')
+# plt.gca().add_patch(idealPsf)
+# # ax.set_aspect('equal')
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.legend()
+# plt.title("Elliptic Gaussian Fits")
+# plt.grid()
+# plt.show()
+
+# plt.scatter(exp_x, exp_y, c=exp_z, cmap='viridis')
+# plt.scatter(0,0,c="red")
+# for i in np.linspace(.2e-6,2e-6,10):
+# 	circle = plt.Circle((0, 0), radius=i, color='orange', fill=False, linestyle='--')
+# 	plt.gca().add_patch(circle)
+# plt.legend()
+
 
 '''----------------------------------------azimutal average----------------------------------------'''
 
 plt.figure("azimutal average")
-names = ["5us", "6us", "10us", "12us"]
-for coords, name in zip(coordinates, names):
-	r,az = Camera.cameraAtomImages.azimuthal_average(*coords,(0,0),)
-	az /= np.max(az)
-	plt.plot(r,az, label = name)
-blur_z0,metadata = Camera.load_h5_image("d:/simulationImages/blurs/399nm/camera_atomZ=0.00e+00.h5", returnMetadata=True)
-blur_z0 = np.abs(blur_z0)**2
-blur_z0 = blur_z0[blur_z0.shape[0]//2,blur_z0.shape[1]//2:]/np.sum(blur_z0)
-offset=.00
-higherValue = 1
-# plt.plot(np.linspace(0,metadata["range"]/2/8, len(blur_z0)), blur_z0/np.max(blur_z0)*(higherValue-offset)+offset, label = "blur at z=0")
+# names = ["5us", "12us", "12us_real"]
+for i, (coords, name, valForEllipse) in enumerate(zip(coordinates, names, valuesForEllpiticFit)):
+	if i < len(exp_files):
+		r,az = Camera.cameraAtomImages.elliptical_average(*coords,valForEllipse[2], valForEllipse[0]/valForEllipse[1], (0,0),dividestandardBin=2)
+	else:
+		r,az = Camera.cameraAtomImages.azimuthal_average(*coords,(0,0),dividestandardBin=2)
+	# az /= np.max(az)
+	# Initial guess: amplitude, mean, stddev, offset
+	p0 = [np.max(az), pixelSize, np.min(az)]
+	params, cov = curve_fit(gaussian, r*8, az, p0=p0)
+	print(params)
+	# az = (az-params[-1]) / params[0] + params[-1]
+	# params[0] = 1
+	line, = plt.plot(r*8, az, label=name)
+	plt.plot(r*8, gaussian(r*8, *params), label=f"{name}: gaussian fit: {params[1]*1e9/8:.1f}nm", linestyle=':', color=line.get_color())
+# blur_z0,metadata = Camera.load_h5_image("d:/simulationImages/blurs/399nm/camera_atomZ=0.00e+00.h5", returnMetadata=True)
+# blur_z0 = np.abs(blur_z0)**2
+# blur_z0 = blur_z0[blur_z0.shape[0]//2,:]/np.sum(blur_z0)
+# blur_z0 = blur_z0 / np.max(blur_z0) * 35 * 35/23
+# blurSize = metadata["range"]
+# x = np.linspace(-blurSize/2, (blurSize*(blur_z0.shape[0]-1)/blur_z0.shape[0])-blurSize/2, blur_z0.shape[0])
+# pixel_response = np.where(np.abs(x) <= pixelSize*8/2, 1, 0)
+# plt.plot(x, np.where(np.logical_and(x>=-2e-10,x <= pixelSize*8), 1, 0) * np.max(blur_z0), label='pixel')
+# pixel_response = pixel_response / np.sum(pixel_response)  # Normalize
+# blur_z0_pixel = np.convolve(blur_z0, pixel_response, mode='same')
+# plt.plot(x,blur_z0, label = "blur at z=0 (normalized)")
+# plt.plot(x,blur_z0_pixel, label = "blur convolved with pixel")
+plt.xlabel("radius (m), camera space")
+plt.ylabel("intensity")
+plt.xlim(left=0)
+plt.grid()
 plt.legend()
-plt.title("azimutal average")
+plt.title("comparison with different offsets")
 plt.show()
+
+
+
+exp_fit = Camera.cameraAtomImages.findCenterOfGaussianSetOfImages(coords[0], coords[1], coords[2])
+
 
 '''----------------------------------------3D intensity comparison----------------------------------------'''
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-for coords, name in zip(coordinates, names):
+for coords, name in zip(coordinates[0:1], names[0:1]):
 	x,y,z=Camera.filterScatterToGrid(coords[0].flatten(), coords[1].flatten(), coords[2].flatten(),[comparisonRoi,comparisonRoi])
-	ax.plot_trisurf(x.flatten(),y.flatten(),z.flatten(), label=name, alpha=0.7)
+	ax.plot_trisurf(coords[0].flatten(), coords[1].flatten(), coords[2].flatten(), label=name, alpha=0.7)
 	# ax.scatter(x,y,z, label = name)
 
 plt.legend()
@@ -103,10 +223,10 @@ plt.show()
 
 '''----------------------------------------average intensity comparison----------------------------------------'''
 fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-plt.suptitle("Intensity Comparison", fontsize=16)  # Add an overall title
+plt.suptitle("Intensity Comparison (sub-pixel)", fontsize=16)  # Add an overall title
 xx, yy, zz = [[], [], []]
 for coords, name in zip(coordinates, names):	
-	x, y, z = Camera.filterScatterToGrid(coords[0].flatten(), coords[1].flatten(), coords[2].flatten(), [comparisonRoi, comparisonRoi])
+	x, y, z = Camera.filterScatterToGrid(coords[0].flatten(), coords[1].flatten(), coords[2].flatten(),[comparisonRoi,comparisonRoi])
 	xx.append(x)
 	yy.append(y)
 	zz.append(z)
@@ -200,61 +320,60 @@ for signal, direction in zip([average[:,average.shape[1]//2], average[average.sh
 plt.legend()
 plt.show()
 
-'''histogram of number of photons in roy'''
+# '''histogram of number of photons in roy'''
 
-roi = 3
-metadata = list(metadata.values())[0]
-atomPosition = metadata["atoms_InitialCoordinates"][0]
-atomPosition[0] *=-1
-pixelSize = metadata["camera_pixelSize"]
-pixelNumber = imgs.shape[1]
-halfImageSize = pixelNumber*pixelSize/2
-atomPixels = np.array([(atomPosition[0]+halfImageSize) / pixelSize,
-			  (atomPosition[1]+halfImageSize) / pixelSize]).astype(int) - roi//2
-atomPixels = imgs[:,atomPixels[0]:atomPixels[0]+roi,atomPixels[1]:atomPixels[1]+roi]
-photonsInRoi = np.sum(atomPixels, axis=(1,2))
+# roi = 3
+# metadata = list(metadata.values())[0]
+# atomPosition = metadata["atoms_InitialCoordinates"][0]
+# atomPosition[0] *=-1
+# pixelSize = metadata["camera_pixelSize"]
+# pixelNumber = imgs.shape[1]
+# halfImageSize = pixelNumber*pixelSize/2
+# atomPixels = np.array([(atomPosition[0]+halfImageSize) / pixelSize,
+# 			  (atomPosition[1]+halfImageSize) / pixelSize]).astype(int) - roi//2
+# atomPixels = imgs[:,atomPixels[0]:atomPixels[0]+roi,atomPixels[1]:atomPixels[1]+roi]
+# photonsInRoi = np.sum(atomPixels, axis=(1,2))
 
-plt.hist(photonsInRoi, bins=int(np.max(photonsInRoi)-np.min(photonsInRoi)+1), label="photons in ROI")
+# plt.hist(photonsInRoi, bins=int(np.max(photonsInRoi)-np.min(photonsInRoi)+1), label="photons in ROI")
 
-unique, counts = np.unique(photonsInRoi, return_counts=True)
-params, _ = curve_fit(poisson.pmf, unique, counts / len(photonsInRoi), p0=[np.mean(photonsInRoi)])
-plt.plot(unique, len(photonsInRoi) * poisson.pmf(unique, *params), label=f'Poisson fit (mean = {params[0]})')
-plt.legend()
-plt.show()
+# unique, counts = np.unique(photonsInRoi, return_counts=True)
+# params, _ = curve_fit(poisson.pmf, unique, counts / len(photonsInRoi), p0=[np.mean(photonsInRoi)])
+# plt.plot(unique, len(photonsInRoi) * poisson.pmf(unique, *params), label=f'Poisson fit (mean = {params[0]})')
+# plt.legend()
+# plt.show()
 
-'''comparison with experimental images'''
-from scipy.ndimage import maximum_filter
-with h5py.File("d:/simulationImages/real images/10_tweezers_mean_images.h5", 'r') as f:
-	freeSpaceIllumTimes = np.array(f['Free space illumination times'])
-	firstImageInTrap = np.array(f['first image in trap (6us ill time) average'])
-	secondImageFreeSpace = np.array(f['second image free space average'])
-	secondImageFreeSpace_Notes = dict(f['second image free space average'].attrs)
+# '''comparison with experimental images'''
+# with h5py.File("d:/simulationImages/real images/10_tweezers_mean_images.h5", 'r') as f:
+# 	freeSpaceIllumTimes = np.array(f['Free space illumination times'])
+# 	firstImageInTrap = np.array(f['first image in trap (6us ill time) average'])
+# 	secondImageFreeSpace = np.array(f['second image free space average'])
+# 	secondImageFreeSpace_Notes = dict(f['second image free space average'].attrs)
 
-roiFromSimulations = np.mean(atomPixels, axis=0)
-roiFromData = getAverageRoi(secondImageFreeSpace[0], roi)
-difference = roiFromData - roiFromSimulations
+# roiFromSimulations = np.mean(atomPixels, axis=0)
+# roiFromData = getAverageRoi(secondImageFreeSpace[0], roi)
+# difference = roiFromData - roiFromSimulations
 
-print(f"average photons in simulation roi: {np.sum(roiFromSimulations)}")
-print(f"average photons in experiment roi: {np.sum(roiFromData)}")
+# print(f"average photons in simulation roi: {np.sum(roiFromSimulations)}")
+# print(f"average photons in experiment roi: {np.sum(roiFromData)}")
 
-fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-vmin = min(roiFromSimulations.min(), roiFromData.min(), difference.min())
-vmax = max(roiFromSimulations.max(), roiFromData.max(), difference.max())
+# fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+# vmin = min(roiFromSimulations.min(), roiFromData.min(), difference.min())
+# vmax = max(roiFromSimulations.max(), roiFromData.max(), difference.max())
 
-im0 = axes[0].imshow(roiFromSimulations, vmin=vmin, vmax=vmax)
-axes[0].set_title("ROI from Simulations")
-im1 = axes[1].imshow(roiFromData, vmin=vmin, vmax=vmax)
-axes[1].set_title("ROI from Data")
-im2 = axes[2].imshow(difference, vmin=vmin, vmax=vmax)
-axes[2].set_title("Difference (Data - Simulations)")
+# im0 = axes[0].imshow(roiFromSimulations, vmin=vmin, vmax=vmax)
+# axes[0].set_title("ROI from Simulations")
+# im1 = axes[1].imshow(roiFromData, vmin=vmin, vmax=vmax)
+# axes[1].set_title("ROI from Data")
+# im2 = axes[2].imshow(difference, vmin=vmin, vmax=vmax)
+# axes[2].set_title("Difference (Data - Simulations)")
 
-fig.colorbar(im0, ax=axes, orientation='vertical', fraction=0.02, pad=0.04)
-plt.show()
+# fig.colorbar(im0, ax=axes, orientation='vertical', fraction=0.02, pad=0.04)
+# plt.show()
 
-plt.plot(*getAverageCrossSection(firstImageInTrap), label="averaged data")
-plt.plot(*get_xyFromSignal(average[average.shape[0]//2,:]), label="simulation")
-plt.legend()
-plt.show()
+# plt.plot(*getAverageCrossSection(firstImageInTrap), label="averaged data")
+# plt.plot(*get_xyFromSignal(average[average.shape[0]//2,:]), label="simulation")
+# plt.legend()
+# plt.show()
 
-# for i in range(len(secondImageFreeSpace)):
-#     Camera.save_h5_image(f"D:/simulationImages/real images/2ndImagefreeSpace_{freeSpaceIllumTimes[i]:.1e}.h5", secondImageFreeSpace[i], illumTime = freeSpaceIllumTimes[i], **secondImageFreeSpace_Notes)
+# # for i in range(len(secondImageFreeSpace)):
+# #     Camera.save_h5_image(f"D:/simulationImages/real images/2ndImagefreeSpace_{freeSpaceIllumTimes[i]:.1e}.h5", secondImageFreeSpace[i], illumTime = freeSpaceIllumTimes[i], **secondImageFreeSpace_Notes)

testDiffraction_fourthAttempt.py
@@ -100,7 +100,7 @@ def passThroughLens(U0, k,f,R):
 
 '''-------------------------------------------------expansion formulas---------------------------------------------------------'''
 def h(xyz, k):
-	#'''
+	# '''
 	return k/(2j*np.pi*xyz[...,2]), k*xyz[...,2] + .5*k/xyz[...,2]*(xyz[...,0]**2+xyz[...,1]**2)
 	'''
 	r=np.linalg.norm(xyz,axis=-1)
@@ -199,72 +199,82 @@ lam = 399e-9#1e-6#f0/3#399e-9
 k=2*np.pi/lam
 
 pixelSize = 4.6e-6
-requestedRange = pixelSize * 8
-resolution = 150
-range_beforeLens1 = R1*2
-rangeForFft = resolution * lam * f1 / requestedRange
+requestedRange = pixelSize * 16
+resolution_beforeLens1 = 150
+resolution_objective = 150
+range_beforeLens1 = R1*2#15e-3 * 0.07
+rangeForFft = resolution_objective * lam * f1 / requestedRange
 
 metadata = {
-	"f0" 				: f0,
-	"pow" 				: pow,
-	"R0" 				: R0,
-	"f1" 				: f1,
-	"R1" 				: R1,
-	"lam" 				: lam,
-	"k" 				: k,
-	"pixelSize"			: pixelSize,
-	"requestedRange"	: requestedRange,
-	"resolution"		: resolution,
-	"range_beforeLens1"	: range_beforeLens1,
-	"rangeForFft"		: rangeForFft,
+	"f0" 						: f0,
+	"pow" 						: pow,
+	"R0" 						: R0,
+	"f1" 						: f1,
+	"R1" 						: R1,
+	"lam" 						: lam,
+	"k" 						: k,
+	"pixelSize"					: pixelSize,
+	"requestedRange"			: requestedRange,
+	"resolution_objective"		: resolution_objective,
+	"resolution_beforeLens1"	: resolution_beforeLens1,
+	"range_beforeLens1"			: range_beforeLens1,
+	"rangeForFft"				: rangeForFft,
 }
+beforeLens_path = "D:/simulationImages/blurs/399nm/beforeLens/"
+objective_path = "D:/simulationImages/blurs/399nm/largerBlur/"
 
+if not os.path.exists(beforeLens_path):
+    os.makedirs(beforeLens_path)
+if not os.path.exists(objective_path):
+    os.makedirs(objective_path)
 
-zAtom = np.linspace(-2e-6, 2e-6, 41)
-contour = np.zeros((len(zAtom), resolution))
+zAtom = np.linspace(-8e-6, 8e-6, 21)
+contour = np.zeros((len(zAtom), resolution_objective))
 for i,z in enumerate(zAtom):
 	
-	beforLens_file_name = f"D:/simulationImages/blurs/399nm/beforeLens/beforeLens1_atomZ={z:.2e}.h5"
+	beforLens_file_name = f"{beforeLens_path}beforeLens1_atomZ={z:.2e}.h5"
 	if not os.path.exists(beforLens_file_name):
 		metadata["zLens0"] = zLens0=f0+z
-		metadata["zLens1"] = zLens1=zLens0+f0+f1
+		metadata["zLens1"] = zLens1=zLens0+600e-3#f0+f1
 		metadata["zObjective"] = zObjective=zLens1+f1
 
 		U_beforeLens0 = dipoleField(k)
 		U_afterLens0 = passThroughLens(U_beforeLens0, k, f0, R0)
+		plot2D_function(addStaticY(toComplex(U_beforeLens0),0),[0,R0*1.1],[f0*.5,f0],40,40, "U_beforeLens0_xz")
+		plot2D_function(addStaticZ(toComplex(U_afterLens0),zLens0),[0,R0*1.1],[0,R0*1.1],40,40, "U_beforeLens0_xz")
 
 		U_beforeLens1 = expandInFreeSpace(U_afterLens0,[[-R0,R0],[-R0,R0]],zLens0,k)
-		# plot2D_function(addStaticY(U_beforeLens1,0), [0,R0*1.1],[zLens0 + .5*f1, zLens0 + 1.5*f1],20,20, "U_beforeLens1_xz")
-		# plot2D_function(addStaticZ(U_beforeLens1,zLens1), [0,R0*1.1],[0,R0*1.1],20,20, "U_beforeLens1_xy")
+		plot2D_function(addStaticY(U_beforeLens1,0), [0,R1*1.1],[zLens0 + .5*f1, zLens0 + 1.5*f1],20,20, "U_beforeLens1_xz")
+		plot2D_function(addStaticZ(U_beforeLens1,zLens1), [0,R1*1.1],[0,R1*1.1],3,3, "U_beforeLens1_xy")
 
-		mappedFun_beforeLens1 = mappedFunction(addStaticZ(U_beforeLens1,zLens1), np.array([0,0]), np.repeat(range_beforeLens1, 2), np.repeat(resolution,2))
+		mappedFun_beforeLens1 = mappedFunction(addStaticZ(U_beforeLens1,zLens1), np.repeat(0, 2), np.repeat(range_beforeLens1, 2), np.repeat(resolution_beforeLens1,2))
 		U_beforeLens1_calculated = mappedFun_beforeLens1(mappedFun_beforeLens1.getXY())
 		# plot2D(U_beforeLens1_calculated,[-range_beforeLens1/2,range_beforeLens1/2],[-range_beforeLens1/2,range_beforeLens1/2], "fieldAtObjective")
-		
+
 		save_h5_image(beforLens_file_name, U_beforeLens1_calculated, zAtom = z, range = range_beforeLens1, **metadata)
 
-	objective_file_name = f"D:/simulationImages/blurs/399nm/camera_atomZ={z:.2e}.h5"
+	objective_file_name = f"{objective_path}camera_atomZ={z:.2e}.h5"
 	if not os.path.exists(objective_file_name):
 		#let's get it from the file
 		U_beforeLens1_calculated, loadedMetadata = load_h5_image(beforLens_file_name, returnMetadata=True)
-
+		# U_beforeLens1_calculated = mirrorSymmetricImage(U_beforeLens1_calculated)
 		Range = loadedMetadata['range']
 		loadedMetadata.pop('range', None)
 		loadedMetadata.pop('resolution', None)
 
-		U_objective = fftshift(fft2_butBetter(U_beforeLens1_calculated, np.repeat(Range,2), np.repeat(requestedRange / (lam * f1), 2), transform_n=np.repeat(resolution,2)))
+		U_objective = fftshift(fft2_butBetter(U_beforeLens1_calculated, np.repeat(Range,2), np.repeat(requestedRange / (lam * f1), 2), transform_n=np.repeat(resolution_objective,2)))
 		
-		save_h5_image(objective_file_name, U_objective, range = requestedRange, resolution = resolution, **loadedMetadata)
+		save_h5_image(objective_file_name, U_objective, range = requestedRange, resolution = resolution_objective, **loadedMetadata)
 	U_objective = load_h5_image(objective_file_name)
 
-	contour[i] = np.abs(U_objective[int(resolution/2),:])
+	contour[i] = np.abs(U_objective[int(resolution_objective/2),:])
 
 	# objective_Ray = resolution[0] * lam * f1 / Range[0] / 2
 	# plot2D(U_objective,[-requestedRange/2,requestedRange/2],[-requestedRange/2,requestedRange/2], "fieldAtObjective")
 
 
-plot2D(contour,[-requestedRange/2,requestedRange/2],[-2e-6,2e-6], "fieldAtObjective")
-plot2D(np.log10(contour),[-requestedRange/2,requestedRange/2],[-2e-6,2e-6], "fieldAtObjective")
+plot2D(contour,[-requestedRange/2,requestedRange/2],[zAtom.min(),zAtom.max()], "fieldAtObjective")
+# plot2D(np.log10(contour),[-requestedRange/2,requestedRange/2],[-2e-6,2e-6], "fieldAtObjective")
 
 
 '''-----------------------double lens with gaussian beam----------------------'''

testingBeamPlots.py
@@ -53,11 +53,14 @@ if usedCase == fixedAtom:
     trajlib.experiment.nextPointInTrajectory = lambda *x,**y: None
 
 '''-------------------------------pixels and camera------------------------------'''
-pixelScale =                                                                        10.
-pixelType = "" if pixelScale == 1 else f"_{pixelScale}reduction"
+pixelScale = 1#                                                                       10.
+pixelType = "" if pixelScale == 1 else f"_{int(pixelScale)}reduction"
 nPixels = np.array([20,20 * ((nOfAtoms+1)//2)]) * int(pixelScale)
 pixelSize = 4.6e-6 / pixelScale
+magnification = 8
 cameraSize = pixelSize*nPixels
+atomSpaceSize = cameraSize / magnification
+defocus = 0e-6
 lensDistance = 25.5e-3
 lensRadius = 16e-3
 quantumEfficiency = .83
@@ -66,17 +69,18 @@ dicroicReflection = .9
 filterTransmission = .9**2
 totalEfficiency = quantumEfficiency * objectiveTransmission * dicroicReflection * filterTransmission
 
+
 '''-------------------------------timings------------------------------'''
 max_dt = 1e-8
 min_dt = 1e-10
 max_dx = twzWaist / 10
 impulseDuration = 400e-9
-experimentDuration = 12e-6 #2-50us
-acquisitionDuration = 10e-6#experimentDuration * 1#if you want, you can do more than one acquisition time for the same simulation (of course the simulation has to be long enough)
+experimentDuration = 20e-6 #2-50us
+acquisitionDuration = 20e-6#experimentDuration * 1#if you want, you can do more than one acquisition time for the same simulation (of course the simulation has to be long enough)
 
 '''-------------------------------folders and files------------------------------'''
-pictureFolder = f"D:/simulationImages/correctScattering_Yt{isotope}_{int(experimentDuration*1e6)}us_{nOfAtoms}tweezerArray{usedCase}/{int(acquisitionDuration*1e6)}us_pictures{pixelType}/"
-simulationFolder = f"D:/simulationImages/correctScattering_Yt{isotope}_{int(experimentDuration*1e6)}us_{nOfAtoms}tweezerArray{usedCase}/simulation/"
+pictureFolder = f"D:/simulationImages/magnified_Yt{isotope}_{int(experimentDuration*1e6)}us_{nOfAtoms}tweezerArray{usedCase}/correctMagnification_outOfFocus{defocus:.1e}m_{int(acquisitionDuration*1e6)}us_pictures{pixelType}/"
+simulationFolder = f"D:/simulationImages/magnified_Yt{isotope}_{int(experimentDuration*1e6)}us_{nOfAtoms}tweezerArray{usedCase}/simulation/"
 baseFileName = "simulation"
 blurFolder = "D:/simulationImages/blurs/399nm"
 
@@ -90,10 +94,10 @@ if not os.path.exists(simulationFolder):
 if nOfAtoms==1:
     tweezerBaseCenter = np.zeros((1,3))
 else:
-    tweezerBaseCenter = np.stack((np.linspace(cameraSize[1]*.4,-cameraSize[1]*.4,nOfAtoms), np.linspace(-cameraSize[0]*.1,cameraSize[0]*.1,nOfAtoms), np.zeros(nOfAtoms)), axis=-1)
+    tweezerBaseCenter = np.stack((np.linspace(atomSpaceSize[1]*.4,-atomSpaceSize[1]*.4,nOfAtoms), np.linspace(-atomSpaceSize[0]*.1,atomSpaceSize[0]*.1,nOfAtoms), np.zeros(nOfAtoms)), axis=-1)
 
 if useFixedTweezers:
-    tweezerBaseCenter += np.random.random(tweezerBaseCenter.shape)*np.array([-pixelSize,pixelSize,0])
+    tweezerBaseCenter += (np.random.random(tweezerBaseCenter.shape) * np.array([-pixelSize,pixelSize,0]) / magnification)
 
 def elasticForceFromTweezer(a : trajlib.Atom, *args):
     x,y,z = a.position - a.tweezerPosition
@@ -108,9 +112,9 @@ def elasticForceFromTweezer(a : trajlib.Atom, *args):
 
 G = Camera.blurFromImages(blurFolder)
 #'''
-magnificationGrid = pixelGrid(cameraSize[0],cameraSize[1],nPixels[0],nPixels[1], lambda xy, z: G(xy, z - lensDistance))
+magnificationGrid = pixelGrid(cameraSize[0],cameraSize[1],nPixels[0],nPixels[1], lambda xy, z: G(xy, z - lensDistance), magnification=magnification)
 quantumEfficiencyGrid = refreshing_cMosGrid(cameraSize[0],cameraSize[1],nPixels[0],nPixels[1], randExtractor.randomLosts(1-totalEfficiency), "Orca_testing/shots/", imageStart = (10,10), imageSizes = (-10,-10))
-c = Camera(position=(0,0,lensDistance), 
+c = Camera(position=(0,0,lensDistance + defocus), 
         orientation=(0,-np.pi/2,0), 
         radius=lensRadius,
         pixelGrids=(magnificationGrid, quantumEfficiencyGrid))
@@ -162,7 +166,9 @@ for repeat in range(1000):
             saturation = saturation,
 
             lens0_radius = c.radius,
-            lens0_distance = lensDistance,
+            lens0_distance = lensDistance + defocus,
+            defocus = defocus,
+            magnification = magnification,
             quantumEfficiency = quantumEfficiency,
             objectiveTransmission = objectiveTransmission,
             dicroicReflection = dicroicReflection,
@@ -172,6 +178,7 @@ for repeat in range(1000):
             camera_pixelSize = pixelSize,
             camera_pixelNumber = nPixels,
             camera_blurImagesPath = blurFolder,
+
             tweezer_waist = twzWaist,
             tweezer_intensity = twzIntensity,
             tweezer_lambda = twzLambda,
@@ -182,6 +189,20 @@ for repeat in range(1000):
         exp.saveAcquisition(simulationFileName, **metadata)
     else:
         metadata = exp.loadAcquisition(simulationFileName)
+        metadata.update(dict(
+            lens0_radius = c.radius,
+            lens0_distance = lensDistance,
+            quantumEfficiency = quantumEfficiency,
+            objectiveTransmission = objectiveTransmission,
+            dicroicReflection = dicroicReflection,
+            filterTransmission = filterTransmission,
+            totalEfficiency = totalEfficiency,
+            camera_pixelSize = pixelSize,
+            camera_pixelNumber = nPixels,
+            camera_blurImagesPath = blurFolder,
+        ))
+        if useFixedTweezers:
+            tweezerBaseCenter = metadata["tweezer_centers"]
     if showPlots:
         exp.plotTrajectories()
     if not os.path.exists(imageFileName):